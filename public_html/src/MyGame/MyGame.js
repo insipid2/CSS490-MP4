5e3eb22dfc279950c8cecd5dd9a98fc42d784c5a
Brian <blorton@gmail.com>
2017-02-11 17:08:20 -0800

    Page, Canvas, Cameras
    set page title, set icon. set canvas size to 800x800. set up 5 cameras:
    main view on bottom, 4 zoomed views across top.

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -12,8 +12,14 @@
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
 function MyGame() {
-    // The camera to view the scene
-    this.mCamera = null;
+    // Main - large bottom view
+    // Hero - always centered on Hero
+    // DyeHit1-3 - for focusing on DyePack collisions
+    this.mCameraMain = null;
+    this.mCameraHero = null;
+    this.mCameraDyeHit1 = null;
+    this.mCameraDyeHit2 = null;
+    this.mCameraDyeHit2 = null;
 
     this.mMsg = null;
 
@@ -25,13 +31,41 @@ gEngine.Core.inheritPrototype(MyGame, Scene);
 
 MyGame.prototype.initialize = function () {
     // Step A: set up the cameras
-    this.mCamera = new Camera(
+    this.mCameraMain = new Camera(
         vec2.fromValues(30, 27.5), // position of the camera
         100,                       // width of camera
-        [0, 0, 640, 480]           // viewport (orgX, orgY, width, height)
+        [0, 0, 800, 600]           // viewport (orgX, orgY, width, height)
     );
-    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+    this.mCameraMain.setBackgroundColor([0.8, 0.8, 0.8, 1]);
             // sets the background to gray
+    
+    this.mCameraHero = new Camera(
+        vec2.fromValues(30, 27.5), // position of the camera
+        100,                       // width of camera
+        [0, 600, 200, 200]           // viewport (orgX, orgY, width, height)
+    );
+    this.mCameraHero.setBackgroundColor([0.8, 0.8, 0.6, 1]);
+            
+    this.mCameraDyeHit1 = new Camera(
+        vec2.fromValues(30, 27.5), // position of the camera
+        100,                       // width of camera
+        [200, 600, 200, 200]           // viewport (orgX, orgY, width, height)
+    );
+    this.mCameraDyeHit1.setBackgroundColor([0.8, 0.6, 0.8, 1]);
+            
+    this.mCameraDyeHit2 = new Camera(
+        vec2.fromValues(30, 27.5), // position of the camera
+        100,                       // width of camera
+        [400, 600, 200, 200]           // viewport (orgX, orgY, width, height)
+    );
+    this.mCameraDyeHit2.setBackgroundColor([0.6, 0.8, 0.8, 1]);
+            
+    this.mCameraDyeHit3 = new Camera(
+        vec2.fromValues(30, 27.5), // position of the camera
+        100,                       // width of camera
+        [600, 600, 200, 200]           // viewport (orgX, orgY, width, height)
+    );
+    this.mCameraDyeHit3.setBackgroundColor([0.6, 0.8, 0.6, 1]);
 
     this.mMsg = new FontRenderable("Status Message");
     this.mMsg.setColor([0, 0, 0, 1]);
@@ -45,13 +79,18 @@ MyGame.prototype.draw = function () {
     // Step A: clear the canvas
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
-    this.mCamera.setupViewProjection();
+    this.mCameraMain.setupViewProjection();
     var i, l;
     for (i = 0; i < this.mLineSet.length; i++) {
         l = this.mLineSet[i];
-        l.draw(this.mCamera);
+        l.draw(this.mCameraMain);
     }
-    this.mMsg.draw(this.mCamera);   // only draw status in the main camera
+    this.mMsg.draw(this.mCameraMain);   // only draw status in the main camera
+    
+    this.mCameraHero.setupViewProjection();
+    this.mCameraDyeHit1.setupViewProjection();
+    this.mCameraDyeHit2.setupViewProjection();
+    this.mCameraDyeHit3.setupViewProjection();
 };
 
 // The Update function, updates the application state. Make sure to _NOT_ draw
@@ -61,21 +100,10 @@ MyGame.prototype.update = function () {
     var echo = "";
     var x, y;
 
-    if (gEngine.Input.isButtonPressed(gEngine.Input.mouseButton.Middle)) {
-        var len = this.mLineSet.length;
-        if (len > 0) {
-            this.mCurrentLine = this.mLineSet[len - 1];
-            x = this.mCamera.mouseWCX();
-            y = this.mCamera.mouseWCY();
-            echo += "Selected " + len + " ";
-            echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
-            this.mCurrentLine.setFirstVertex(x, y);
-        }
-    }
 
     if (gEngine.Input.isButtonPressed(gEngine.Input.mouseButton.Left)) {
-        x = this.mCamera.mouseWCX();
-        y = this.mCamera.mouseWCY();
+        x = this.mCameraMain.mouseWCX();
+        y = this.mCameraMain.mouseWCY();
         echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
 
         if (this.mCurrentLine === null) { // start a new one
